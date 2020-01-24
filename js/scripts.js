15aabd9a8e9d8f64cfb37074c4d97c019435c7f0
Tiffany Siu <tsiu88@gmail.com>
2020-01-24 09:27:28 -0800

    add loop to return range of numbers without modifications

--- js/scripts.js
+++ js/scripts.js
@@ -16,15 +16,22 @@ $(document).ready(function(){
     event.preventDefault();
 
     var userInput = $("#inputNumber").val();
-
     var isANumber = numberCheck(userInput);
-    
+    var numberArray = [];
+
     if (isANumber){
       
+      for(var i=0; i <=userInput; i++){
+        numberArray.push(i);
+      }
+
     }
     else{
       alert("No number inputted.  Please enter a number in space provided.");
     }
+
+    $(".outputArea").show();
+    $("#output").text(numberArray);
 
   });
 });
